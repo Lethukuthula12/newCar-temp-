463c3c1314bd1319eeea3b62a2c75f4a94170401
Lethukuthula <u17241414@tuks.co.za>
2020-10-29 08:39:40 +0200

    made contacts, collection,signin and signout, showcase components responsive

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -9,9 +9,9 @@ const CollectionPreview = ({ title, items }) => (
     <h1 className='title'>{title.toUpperCase()}</h1>
     <div className='preview'>
       {items
-        .filter((item, idx) => idx < 4)
+        .filter((item, idx) => idx < 3)
         .map(item => (
-          <CollectionItem key={item.id} item={item} />
+          <CollectionItem  key={item.id} item={item} />
         ))}
     </div>
   </div>
